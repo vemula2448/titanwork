23c212a98c7397b9bb78eb06e3888c321a1d2d94
vemula2448 <vemulapraveen2448@gmail.com>
2025-10-26 16:54:28 +0530

    modified the file with updated script

--- vemula1.py
+++ vemula1.py
@@ -5,6 +5,7 @@ class Hello
         int x=5;
         int y=10;
        int result =x+y;
-        System.out.println(result);
+        System.out.println(x);
+        Syetem.out.println(y);
     }
 }
